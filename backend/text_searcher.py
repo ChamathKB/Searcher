8262fefa4faad4cbe4f8c6ddca2d21739e06b9ba
ChamathKB <chamathkbatta@gmail.com>
2024-02-16 00:27:49 +0530

    feat(backend): add text and neural search components

---
+++ backend/text_searcher.py
@@ -0,0 +1,47 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import Filter, FieldCondition, MatchText
+from .config import QDRANT_URL, QDRANT_API_KEY, TEXT_FIELD_NAME
+
+import re
+
+
+class TextSearcher:
+    def __init__(self, collection_name: str):
+        self.highlight_field = TEXT_FIELD_NAME
+        self.collection_name = collection_name
+        self.qdrant_client = QdrantClient(url=QDRANT_URL, api_key=QDRANT_API_KEY, prefer_grpc=True)
+
+    def highlight(self, record, query) -> dict:
+        """ Highlights the query in the text field. 
+        """
+        text = record[self.highlight_field]
+
+        for word in query.lower().split():
+            if len(word) > 4:
+                pattern = re.compile(fr"(\b{re.escape(word)}?.?\b)", flags=re.IGNORECASE)
+            else:
+                pattern = re.compile(fr"(\b{re.escape(word)}\b)", flags=re.IGNORECASE)
+            text = re.sub(pattern, r"<b>\1</b>", text)
+
+        record[self.highlight_field] = text
+        return record
+
+
+    def search(self, query, top=5):
+        """ Searches for documents containing the query.
+        Returns a list of documents.
+        """
+        hits = self.qdrant_client.scroll(
+            collection_name=self.collection_name,
+            scroll_filter=Filter(
+                must=[
+                    FieldCondition(
+                        key=TEXT_FIELD_NAME,
+                        match=MatchText(text=query),
+                    )
+                ]),
+            with_payload=True,
+            with_vectors=False,
+            limit=top
+        )
+        return [self.highlight(hit.payload, query) for hit in hits[0]]
